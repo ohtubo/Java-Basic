801d939ff71bb23bafbacd558221c7cdd954375f
ohtubo <84015533+ohtubo@users.noreply.github.com>
2021-10-03 11:02:42 +0900

    [Add]_chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		String my_name = "ootubo";
+		final int LUCKY_NUMBER = 2;
+		
+		System.out.println(my_name);
+		
+		my_name = "明日世界が終わりになろうとも、私はリンゴの木を植える";
+		
+		System.out.println(my_name);
+		
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ JavaWcp/src/Variable02.java
@@ -0,0 +1,12 @@
+
+public class Variable02 {
+	
+	public static void main(String[] args) {
+		//定数の宣言
+		final int MIN_NUMBE = 10;
+		System.out.println(MIN_NUMBE);
+		
+		//再代入するとエラー
+		//MIN_NUMBER = 20;
+	}
+}
