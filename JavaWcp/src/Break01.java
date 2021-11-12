9d54cb87a749658d8875fffa5ea2d3a701f41025
ohtubo <84015533+ohtubo@users.noreply.github.com>
2021-11-12 10:36:12 +0900

    [Add]_eclipse _git_practice[1]

--- JavaWcp/src/Break01.java
+++ JavaWcp/src/Break01.java
@@ -1,9 +1,9 @@
 
-
+// 繰り返し処理の練習
 public class Break01 {
 	public static void main(String[] args) {
 		for (int count = 0; count < 5; count++) {
-			if (count == 2) {
+			if (count == 4) {
 				break;
 			}
 			System.out.println("Break01 = " + count);
